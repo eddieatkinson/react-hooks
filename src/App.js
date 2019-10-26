4495460201a3d3ae9437dec65fa0a233160e3a8e
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-10-26 19:07:14 -0400

    'App.js' is now using hooks and not class

--- src/App.js
+++ src/App.js
@@ -4,44 +4,43 @@ import CharPicker from './components/CharPicker';
 import Character from './components/Character';
 
 const App = props => {
-  const [state, setState] = useState({
-    selectedCharacter: 1,
-    side: 'light',
-    destroyed: false
-  });
+
+  const [selectedCharacter, setSelectedCharacter] = useState('1');
+  const [side, setSide] = useState('light');
+  const [destroyed, setDestroyed] = useState(false);
 
   const sideHandler = side => {
-    this.setState({ side: side });
+    setSide(side);
   };
 
   const charSelectHandler = event => {
     const charId = event.target.value;
-    this.setState({ selectedCharacter: charId });
+    setSelectedCharacter(charId);
   };
 
   const destructionHandler = () => {
-    this.setState({ destroyed: true });
+    setDestroyed(true);
   };
 
   let content = (
     <React.Fragment>
       <CharPicker
-        side={this.state.side}
-        selectedChar={this.state.selectedCharacter}
-        onCharSelect={this.charSelectHandler}
+        side={side}
+        selectedChar={selectedCharacter}
+        onCharSelect={charSelectHandler}
       />
-      <Character selectedChar={this.state.selectedCharacter} />
-      <button onClick={this.sideHandler.bind(this, 'light')}>
+      <Character selectedChar={selectedCharacter} />
+      <button onClick={sideHandler.bind(this, 'light')}>
         Light Side
       </button>
-      <button onClick={this.sideHandler.bind(this, 'dark')}>Dark Side</button>
-      {this.state.side === 'dark' && (
-        <button onClick={this.destructionHandler}>DESTROY!</button>
+      <button onClick={sideHandler.bind(this, 'dark')}>Dark Side</button>
+      {side === 'dark' && (
+        <button onClick={destructionHandler}>DESTROY!</button>
       )}
     </React.Fragment>
   );
 
-  if (this.state.destroyed) {
+  if (destroyed) {
     content = <h1>Total destruction!</h1>;
   }
   return content;
